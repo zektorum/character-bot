17f7fc262e2521a0005b3a4a254d2dcc7150e13b
Murad Murtuzaliev <mur.murtuzaliev@yandex.ru>
2022-10-23 19:37:39 +0300

    fix: change constant to environment variable and other fixes

--- character_bot/bot.py
+++ character_bot/bot.py
@@ -1,16 +1,20 @@
+import os
+
 import discord
 from discord.ext import commands
 
-from message import Message
+from character_bot.message import Message
 
 
 class Bot(commands.Bot):
-    def __init__(self, prefix=None):
+    def __init__(self, name, prefix=None):
         intents = discord.Intents().default()
         intents.message_content = True
         super().__init__(command_prefix=prefix, intents=intents)
-        print("I was created")
+        self._name = name
+
+    def get_name(self) -> str:
+        return self._name
 
     async def send_message(self, message: Message):
-        print("In send_message")
-        await super().get_channel(1028037599534780488).send(message.get_message_text())
+        await super().get_channel(int(os.getenv("CHANNEL_ID"))).send(message.get_message_text())
