c4d66d0d504f253920f713d55bf6fc6ee1a66c6c
Murad Murtuzaliev <mur.murtuzaliev@yandex.ru>
2022-10-23 19:36:15 +0300

    feat: add event loop with command which sends dialog

--- character_bot/main.py
+++ character_bot/main.py
@@ -1,2 +1,35 @@
+import asyncio
+import os
+
+import dotenv
+
+from character_bot.bot import Bot
+from character_bot.bot_manager import BotManager
+from character_bot.parser import Parser
+
+
+dotenv.load_dotenv()
+first_bot = Bot(name=os.getenv("KIVY_NAME"), prefix="!")
+second_bot = Bot(name=os.getenv("HORSE_NAME"), prefix="&")
+third_bot = Bot(name=os.getenv("SPY_NAME"), prefix="%")
+
+
+@second_bot.command()
+async def start_dialog(ctx):
+    html_doc = ""
+    with open("messages.html", "r") as file:
+        html_doc = "".join(file.read())
+    chat_history = Parser(html_doc).get_messages()
+    bot_manager = BotManager(chat_history, first_bot, second_bot, third_bot)
+    await bot_manager.send_chat_history()
+
+
 if __name__ == "__main__":
-    pass
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    loop.create_task(first_bot.start(token=os.getenv("KIVY")))
+    loop.create_task(second_bot.start(token=os.getenv("HORSE")))
+    loop.create_task(third_bot.start(token=os.getenv("SPY")))
+
+    loop.run_forever()
